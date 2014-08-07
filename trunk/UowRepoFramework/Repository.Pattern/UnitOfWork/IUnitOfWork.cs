8be93bbae66f8a82cd6ff34ac9a0c96cb16992fa
dnc.dlu@gmail.com <dnc.dlu@gmail.com@e804d79b-2ee0-362f-c3e0-56884ff4ce50>
2014-08-07 14:49:17 +0000

    Genric Uow and Repo Framework

---
+++ trunk/UowRepoFramework/Repository.Pattern/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using Repository.Pattern.Infrastructure;
+using Repository.Pattern.Repositories;
+
+namespace Repository.Pattern.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges();
+        void Dispose(bool disposing);
+        IRepository<TEntity> Repository<TEntity>() where TEntity : class, IObjectState;
+        void BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.Unspecified);
+        bool Commit();
+        void Rollback();
+        dynamic GetCustomRepository<T>();
+        dynamic GetCustomRepository(Type type);
+    }
+}
