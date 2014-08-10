c67fad595d0f36f056540f7dfbeeac68a3a6c538
dnc.dlu@gmail.com <dnc.dlu@gmail.com@e804d79b-2ee0-362f-c3e0-56884ff4ce50>
2014-08-10 02:24:26 +0000

    [uow] service

---
+++ UowRepoFramework/Service/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Model;
+using Repository.Pattern.Repositories;
+using Service.Pattern;
+
+namespace Service
+{
+    public interface IProductService
+    {
+    }
+
+    public class ProductService : Service<Product>, IProductService
+    {
+        public ProductService(IRepositoryAsync<Product> repository)
+            : base(repository)
+        {
+        }
+    }
+}
