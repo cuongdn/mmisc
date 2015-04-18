041b8b5785f89e13aee25396b54a859642a83894
cuongdn <dnc.dlu@gmail.com>
2015-04-19 02:28:16 +0700

    entity framework start up project

---
+++ eftest2/Blogging.Business/Preview/BlogPreviewObjectFactory.cs
@@ -0,0 +1,13 @@
+﻿using Blogging.DbModel.Dto;
+using Core.Business.ObjectFactories;
+
+namespace Blogging.Business.Preview
+{
+    public class BlogPreviewObjectFactory : ObjectFactory<BlogPreview, BlogDto>
+    {
+        public override void Fetch()
+        {
+            base.Fetch();
+        }
+    }
+}
---
+++ eftest2/Blogging.Business/Preview/QuestionPreview.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Blogging.DbModel.Entities;
+using Blogging.DbModel.Repositories;
+using Core.Business.Common;
+using Core.Business.Utils;
+using Core.DataAccess.Repositories;
+
+namespace Blogging.Business.Preview
+{
+    public class QuestionPreview : ModelBase
+    {
+        public override object IdValue
+        {
+            get { return Id; }
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        #region Factory Methods
+
+        // ReSharper disable UnusedMember.Local
+        private void Model_Fetch(Question dbEntity)
+        // ReSharper restore UnusedMember.Local
+        {
+            ObjFacUtil.Fetch(this, dbEntity);
+        }
+
+        public static IList<QuestionPreview> GetList()
+        {
+            var repo = new QuestionRepository(UnitOfWorkFactory.Get());
+            return ModelPortal.FetchChildren<QuestionPreview>(repo.GetAll());
+        }
+
+        #endregion
+    }
+}
