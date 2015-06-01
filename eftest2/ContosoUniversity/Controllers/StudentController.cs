e2912d5b266fa40a68c295ea5871db5d5b01f81f
cuongdn <dnc.dlu@gmail.com>
2015-06-01 12:57:48 +0700

    test

--- eftest2/ContosoUniversity/Controllers/StudentController.cs
+++ eftest2/ContosoUniversity/Controllers/StudentController.cs
@@ -1,9 +1,9 @@
-﻿using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using ContosoUniversity.Infrastructure;
 using Core.Web.Infrastructure;
 using Cs.Business.Preview;
 using Cs.Web.ViewModel;
+using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
 
 namespace ContosoUniversity.Controllers
@@ -11,9 +11,9 @@ namespace ContosoUniversity.Controllers
     public class StudentController : BaseController
     {
         // GET: Student
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
-            return View(await StudentPreview.GetListAsync());
+            return View();
         }
 
         public JsonResult Student_List([DataSourceRequest]DataSourceRequest request)
@@ -58,8 +58,20 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Student/Delete/5
+        public ActionResult Student_Destroy([DataSourceRequest]DataSourceRequest request, int id)
+        {
+            var viewModel = new StudentEditViewModel(id);
+            if (viewModel.Found)
+            {
+                DeleteObject(viewModel.ModelObject, true);
+            }
+            return Json(new[] { viewModel.ModelObject }.ToDataSourceResult(request, ModelState));
+        }
+
+        // GET: Student/Delete/5
         public ActionResult Delete(int id, bool? saveChangesError = null)
         {
+            this.GridRouteValues();
             var viewModel = new StudentEditViewModel(id);
             return ViewDeleteOr404(viewModel, saveChangesError);
         }
