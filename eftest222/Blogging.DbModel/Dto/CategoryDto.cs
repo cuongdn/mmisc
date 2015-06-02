8219c6d2c5d075d62f65c0b9c7eaadffbb216712
cuongdn <dnc.dlu@gmail.com>
2015-06-02 17:34:25 +0700

    sdfas

--- eftest222/Blogging.DbModel/Dto/CategoryDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Blogging.DbModel.Dto
-{
-    public class CategoryDto
-    {
-        public int Id { get; set; }
-        public string CategoryName { get; set; }
-    }
-}
--- eftest222/Cs.Service/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Cs.Service.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
