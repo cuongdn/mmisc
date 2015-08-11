7d4fd9b1ab0f90a26e04e61a27d120e1d8badf4c
Cuong Do <cuongdo926@gmail.com>
2015-08-11 12:14:26 +0700

    scrip db

--- libtest/Cs.ContosoUniversity/Scripts/jquery.validate.js
+++ libtest/Cs.ContosoUniversity/Scripts/jquery.validate.js
@@ -1229,3 +1229,34 @@ $.format = $.validator.format;
 		}
 	});
 }(jQuery));
+
+
+$.validator.setDefaults({
+    errorElement: "span",
+    errorClass: "help-block",
+    highlight: function (element, errorClass, validClass) {
+        if (element.type === "radio") {
+            this.findByName(element.name).addClass(errorClass).removeClass(validClass);
+        } else {
+            $(element).closest('.form-group').removeClass('has-success has-feedback').addClass('has-error has-feedback');
+            $(element).closest('.form-group').find('i.fa').remove();
+            $(element).closest('.form-group').append('<i class="fa fa-exclamation fa-lg form-control-feedback"></i>');
+        }
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        if (element.type === "radio") {
+            this.findByName(element.name).removeClass(errorClass).addClass(validClass);
+        } else {
+            $(element).closest('.form-group').removeClass('has-error has-feedback').addClass('has-success has-feedback');
+            $(element).closest('.form-group').find('i.fa').remove();
+            $(element).closest('.form-group').append('<i class="fa fa-check fa-lg form-control-feedback"></i>');
+        }
+    },
+    errorPlacement: function (error, element) {
+        if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
